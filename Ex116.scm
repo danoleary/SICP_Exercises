388e354f79dfa617e69aef8efaa9c9d9f2bf55bf
Dan O'Leary <daniel@liveposter.com>
2017-09-19 16:23:07 +0100

    completed 1.17

--- Ex116.scm
+++ Ex116.scm
@@ -1,40 +1,20 @@
 (define (even? n)
   (= (remainder n 2) 0))
 
-(define (fast-expt b n)
-  (cond ((= n 0) 1)
-        ((even? n) (square (fast-expt b (/ n 2))))
-        (else (* b (fast-expt b (- n 1))))))
 
-(define (even? n)
-  (= (remainder n 2) 0))
-
-(define (exp n)
-    (define (exp-iter a b n)
-        (cond ((= n 0) a)
-              ((even? n) (exp-iter (* b (/ n 2)) (* n n) (- n 1)))
-              (else (exp-iter (* b (/ n 1)) a (- n 1)))
-        )
+(define (exp b n)
+  (define (iter a b n)
+    (cond ((= n 0) a)
+        ((even? n) (iter a (square b) (/ n 2)))
+        (else (iter (* a b) b (- n 1)))
     )
-    (exp-iter 1 b n)
+  )
+  (iter 1 b n)
 )
 
-(exp-iter 1 2 2)
-(exp-iter 2 2 1)
+(iter 1 2 5)
+(iter 2 2 4)
+(iter 2 4 2)
+(iter 2 16 1)
+(iter 32 16 0)
 
-a   b n
-1 * 2^2 = 4
-4 * b^1 = 4
-n = 0 so return a
-
-(bn/2)2 = (b2)n/2
-
-a * b^n is unchanged
-
-a b^n 
-a=1,b=2,n=2
-2 * 2^2 = 8
-
-a b^n 
-a=1,b=2,n=1
-a b^1 = 8
---
+++ Ex117.scm
@@ -0,0 +1,23 @@
+(define (double x)
+    (* 2 x))
+
+(define (halve x)
+    (/ x 2))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (multiply a b)
+    (define (iter total a b )
+        (cond ((= b 1) total )
+              ((even? b) (iter (+ total (double a)) a (- b 2)))
+              (else (iter (+ total a) a (- b 1)))
+        )   
+    )
+    (iter 0 a b)
+)
+
+(0 6 6)
+(iter 12 6 3)
+(iter 18 6 2)
+(iter 30 6 1)
